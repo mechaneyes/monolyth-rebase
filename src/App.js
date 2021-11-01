257f1909e74dd1d68094c52a7208ad118e0948b5
Ray Weitzenberg <46533657+rayweitzenberg@users.noreply.github.com>
2021-10-31 17:03:12 -0700

    welcome page & toggling to/from + carousel thumbs
    setup welcome page and  rudimentary switching between it and artwork
    pages + shrank carousel thumbs further

--- src/App.js
+++ src/App.js
@@ -1,66 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Header/Header";
+import WelcomePage from "./components/WelcomePage/WelcomePage";
 import Artworks from "./components/Artworks/Artworks";
+
+import './App.scss'
 
 const App = () => {
   const artworks = [
     {
       id: "a1",
-      artist: 'Ray Weitzenberg',
+      artist: "Ray Weitzenberg",
       img: "/artworks/rww-bill-kilgore-4k.gif",
       alt: "Harvey's Angels",
       title: "Harvey's Angels",
       year: 2021,
-      info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod ultrices placerat. Maecenas bibendum molestie dui, pretium rutrum libero aliquet sit amet. Morbi sit amet pharetra libero, vel luctus ligula.',
+      info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod ultrices placerat. Maecenas bibendum molestie dui, pretium rutrum libero aliquet sit amet. Morbi sit amet pharetra libero, vel luctus ligula.",
       qr: "/qr-codes/qr-mechaneyes.com.png",
     },
     {
       id: "a2",
-      artist: 'Ray Weitzenberg',
+      artist: "Ray Weitzenberg",
       img: "/artworks/rww-pacific-design-center.jpg",
       alt: "Pacific Design Center",
       title: "Pacific Design Center",
       year: 2018,
-      info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod ultrices placerat. Maecenas bibendum molestie dui, pretium rutrum libero aliquet sit amet. Morbi sit amet pharetra libero, vel luctus ligula.',
+      info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod ultrices placerat. Maecenas bibendum molestie dui, pretium rutrum libero aliquet sit amet. Morbi sit amet pharetra libero, vel luctus ligula.",
       qr: "/qr-codes/qr-mechaneyes.com.png",
     },
     {
       id: "a3",
-      artist: 'Ray Weitzenberg',
+      artist: "Ray Weitzenberg",
       img: "/artworks/rww-bk-br-style-transfer.png",
       alt: "Brooklyn Bridge Style Transfer",
       title: "Brooklyn Bridge Style Transfer",
       year: 2021,
-      info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod ultrices placerat. Maecenas bibendum molestie dui, pretium rutrum libero aliquet sit amet. Morbi sit amet pharetra libero, vel luctus ligula.',
+      info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod ultrices placerat. Maecenas bibendum molestie dui, pretium rutrum libero aliquet sit amet. Morbi sit amet pharetra libero, vel luctus ligula.",
       qr: "/qr-codes/qr-mechaneyes.com.png",
     },
     {
       id: "a4",
-      artist: 'Ray Weitzenberg',
+      artist: "Ray Weitzenberg",
       img: "/artworks/rww-ft-greene-parkways.jpg",
       alt: "Ft. Greene Parkways",
       title: "Ft. Greene Parkways",
       year: 2021,
-      info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod ultrices placerat. Maecenas bibendum molestie dui, pretium rutrum libero aliquet sit amet. Morbi sit amet pharetra libero, vel luctus ligula.',
+      info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod ultrices placerat. Maecenas bibendum molestie dui, pretium rutrum libero aliquet sit amet. Morbi sit amet pharetra libero, vel luctus ligula.",
       qr: "/qr-codes/qr-mechaneyes.com.png",
     },
     {
       id: "a5",
-      artist: 'Ray Weitzenberg',
+      artist: "Ray Weitzenberg",
       img: "/artworks/rww-hetal-dia-beacon-1920.jpg",
       alt: "Hetal at Dia Beacon",
       title: "Hetal at Dia Beacon",
       year: 2017,
-      info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod ultrices placerat. Maecenas bibendum molestie dui, pretium rutrum libero aliquet sit amet. Morbi sit amet pharetra libero, vel luctus ligula.',
+      info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod ultrices placerat. Maecenas bibendum molestie dui, pretium rutrum libero aliquet sit amet. Morbi sit amet pharetra libero, vel luctus ligula.",
       qr: "/qr-codes/qr-mechaneyes.com.png",
     },
   ];
 
+  const [togglePage, setTogglePage] = useState(false);
+
+  // console.log(togglePage);
+
   return (
     <div>
       <Header />
-      <Artworks items={artworks} />
+      <p className="toggler" onClick={() => setTogglePage(!togglePage)}>{!togglePage ? 'images' : 'welcome'}</p>
+      {!togglePage ? <WelcomePage /> : <Artworks items={artworks} />}
     </div>
   );
 };
---
+++ src/components/WelcomePage/WelcomePage.js
@@ -0,0 +1,21 @@
+import "./WelcomePage.scss";
+
+const WelcomePage = () => {
+  return (
+    <main className="welcome-page">
+      <div className="welcome-page__content">
+        <h2>👋</h2>
+        <h1>Welcome to NFT Kiosk</h1>
+        <h3>
+          Interact with these images by waving your hand over the sensor below.
+        </h3>
+        <h3>
+          Currently you can scroll through the images. Soon you’ll be able to save
+          them to your NFT wallet seamlessly through this interface.
+        </h3>
+      </div>
+    </main>
+  );
+};
+
+export default WelcomePage;
