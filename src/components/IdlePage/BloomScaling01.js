4ba300f80d29623235947f4a0ef83305d100558b
Ray Weitzenberg <ray@mechaneyes.com>
2021-11-10 01:56:21 -0800

    circles animating + new position each loop + alpha
    circles growing and shrinking + when they get to zero width/height the
    array is emptied and repopulated + added transparency

--- src/components/IdlePage/BloomScaling01.js
+++ src/components/IdlePage/BloomScaling01.js
@@ -4,7 +4,6 @@ import Sketch from "react-p5";
 
 // https://p5js.org/examples/transform-scale.html
 
-
 const pacificState = (p5) => {
   let circs = [];
 
@@ -15,42 +14,54 @@ const pacificState = (p5) => {
     "#D87D0F",
     "#A63305",
   ];
-  const fillColor = colorsBubblegum;
   let randoColor;
   let c;
 
-  var a = 0.0
-  let scaler = 0.0
+  var a = 0.0;
+  let scaler = 0.0;
 
   const setup = (p5, canvasParentRef) => {
     p5.frameRate(30);
     p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
     p5.noStroke();
     p5.background("black");
+    
+    generate(p5);
+  };
 
-    for (let i = 0; i < 20; i++) {
-      randoColor = fillColor[Math.floor(Math.random() * fillColor.length)];
+  let generate = (p5) => {
+    circs = [];
+    for (let i = 0; i < 22; i++) {
+      randoColor = colorsBubblegum[Math.floor(Math.random() * colorsBubblegum.length)];
       c = p5.color(randoColor);
+      c.setAlpha(p5.random(160, 255))
       p5.fill(c);
+
       var circle = {
         x: p5.random(p5.windowWidth),
         y: p5.random(p5.windowHeight),
         r: p5.random(50, 100),
-        color: c
-      }
-      circs.push(circle)
+        color: c,
+      };
+      circs.push(circle);
     }
   };
 
   const draw = (p5) => {
-    p5.background('black')
+    p5.background("black");
 
     a = a + 0.03;
-    scaler = p5.cos(a) * 2
+    scaler = p5.cos(a) * 2;
+
+    if (scaler > -0.04 && scaler < 0.04 ) {
+      // console.log('scaler', scaler)
+      generate(p5);
+    }
     
+
     for (let i = 0; i < circs.length; i++) {
-      p5.fill(circs[i].color)
-      p5.circle(circs[i].x, circs[i].y, circs[i].r * scaler)
+      p5.fill(circs[i].color);
+      p5.circle(circs[i].x, circs[i].y, circs[i].r * scaler);
     }
   };
 
