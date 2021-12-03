3b5b1f63732162476d353a8a63cb84eb836c1dd5
Ray Weitzenberg <ray@mechaneyes.com>
2021-12-03 14:54:31 -0800

    homepage copy styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,16 +6,21 @@ const Header = (props) => {
   return (
     <div className="header">
       <div className="header__left">
-        <img
-          className="header__logo"
-          src={`/images/logo_collection.png`}
-          alt="Temporary Logo"
-        />
+        <Link to="/home">
+          <img
+            className="header__logo"
+            src={`/images/logo_collection.png`}
+            alt="Temporary Logo"
+          />
+        </Link>
         <div className="header__title-tagline">
-          <h2>Dwala</h2>
+          <Link to="/home">
+            <h2>Dwala</h2>
+          </Link>
           {/* <p>Waves Hands. Collects Art.</p> */}
           <div className="temp-links">
-          <Link to="/">Idle</Link> | <Link to="/welcome">Welcome</Link> | <Link to="/mechaneyes">Mechaneyes</Link>
+            <Link to="/">Idle</Link> | <Link to="/welcome">Welcome</Link> |{" "}
+            <Link to="/mechaneyes">Mechaneyes</Link>
           </div>
         </div>
       </div>
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -3,18 +3,33 @@ import RandomCrackly from "../RandomCrackly/RandomCrackly";
 import "./Homepage.scss";
 
 let HomepageComp = () => {
-
   return (
     <>
       <Header />
       <main className="homepage">
         <RandomCrackly />
-        <section>
+        <section className="homepage__words">
           {/* https://www.goodreads.com/work/quotes/208362-2001-a-space-odyssey */}
           <h1>Superior Dwala</h1>
-          <h3>The time is fast approaching when Earth, like all mothers, must say farewell to her children.</h3>
-          <p>Unlike the animals, who knew only the present, Man had acquired a past; and he was beginning to grope toward a future.</p>
-          <h2>The truth, as always, will be far stranger.</h2>
+          <h3>
+            The time is fast approaching when Earth, like all mothers, must say
+            farewell to her children.
+          </h3>
+          <p>
+            Unlike the animals, who knew only the present, Man had acquired a
+            past; and he was beginning to grope toward a future. This future
+            still stands as fiction.
+          </p>
+          <h3>The truth, as always, will be far stranger.</h3>
+          <div className="homepage__links">
+            <a href="https://www.instagram.com/theprintfineart/">
+              <p>@theprintfineart</p>
+            </a>{" "}
+            <span className="homepage__spacer">|</span>
+            <a href="https://www.instagram.com/mechaneyes/">
+              <p>@mechaneyes</p>
+            </a>
+          </div>
         </section>
         <video
           className="fiorucci"
