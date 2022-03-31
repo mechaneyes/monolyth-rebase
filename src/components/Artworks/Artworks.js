1d36a86880f67bd7faff13dfc0afd9305baa9de2
Ray Weitzenberg <ray@neoneon.one>
2022-03-31 05:03:18 -0400

    img data file + artworks.js ... images sorted
    images are in place both in the data file and have ordering programmed
    into artworks.js + want to restructure the way thumbnails are managed in
    the second carousel

--- src/components/Artworks/Artworks.js
+++ src/components/Artworks/Artworks.js
@@ -111,16 +111,108 @@ const Artworks = (props) => {
     <>
       <ResetToIdlePage />
       <Slider {...sliderSettings} asNavFor={nav2} ref={sliderArtworks}>
+        {/* alt: "Shaun Burner - Phoenix", */}
         <Artwork
-          artist={props.items[0].artist}
-          image={props.items[0].img}
-          alt={props.items[0].alt}
-          title={props.items[0].title}
-          year={props.items[0].year}
-          info={props.items[0].info}
-          qr={props.items[0].qr}
+          hasVid={props.items[18].vid}
+          artist={props.items[18].artist}
+          image={props.items[18].img}
+          alt={props.items[18].alt}
+          title={props.items[18].title}
+          year={props.items[18].year}
+          info={props.items[18].info}
+          qr={props.items[18].qr}
         />
+        {/* img: "SVwiliums-frogbergcanvas.jpg", */}
         <Artwork
+          hasVid={props.items[16].vid}
+          artist={props.items[16].artist}
+          image={props.items[16].img}
+          alt={props.items[16].alt}
+          title={props.items[16].title}
+          year={props.items[16].year}
+          info={props.items[16].info}
+          qr={props.items[16].qr}
+        />
+        {/* alt: "Camille Martin, Fish", */}
+        <Artwork
+          hasVid={props.items[3].vid}
+          artist={props.items[3].artist}
+          image={props.items[3].img}
+          alt={props.items[3].alt}
+          title={props.items[3].title}
+          year={props.items[3].year}
+          info={props.items[3].info}
+          qr={props.items[3].qr}
+        />
+        {/* img: "Emoni_A_00009_5mp.jpg", */}
+        <Artwork
+          hasVid={props.items[6].vid}
+          artist={props.items[6].artist}
+          image={props.items[6].img}
+          alt={props.items[6].alt}
+          title={props.items[6].title}
+          year={props.items[6].year}
+          info={props.items[6].info}
+          qr={props.items[6].qr}
+        />
+        {/* alt: "Ray Weitzenberg - 33 Thomas Street May Appear", */}
+        <Artwork
+          hasVid={props.items[13].vid}
+          artist={props.items[13].artist}
+          image={props.items[13].img}
+          alt={props.items[13].alt}
+          title={props.items[13].title}
+          year={props.items[13].year}
+          info={props.items[13].info}
+          qr={props.items[13].qr}
+        />
+        {/* alt: "Camille Martin - Peacock", */}
+        <Artwork
+          hasVid={props.items[4].vid}
+          artist={props.items[4].artist}
+          image={props.items[4].img}
+          alt={props.items[4].alt}
+          title={props.items[4].title}
+          year={props.items[4].year}
+          info={props.items[4].info}
+          qr={props.items[4].qr}
+        />
+        {/* alt: "Shaun Burner, Figure Man", */}
+        <Artwork
+          hasVid={props.items[17].vid}
+          artist={props.items[17].artist}
+          image={props.items[17].img}
+          alt={props.items[17].alt}
+          title={props.items[17].title}
+          year={props.items[17].year}
+          info={props.items[17].info}
+          qr={props.items[17].qr}
+        />
+        {/* alt: "Dan Ligaya, Kobe", */}
+        <Artwork
+          hasVid={props.items[5].vid}
+          artist={props.items[5].artist}
+          image={props.items[5].img}
+          alt={props.items[5].alt}
+          title={props.items[5].title}
+          year={props.items[5].year}
+          info={props.items[5].info}
+          qr={props.items[5].qr}
+        />
+        {/* alt: "Nate - LAdy", */}
+        <Artwork
+          hasVid={props.items[12].vid}
+          artist={props.items[12].artist}
+          image={props.items[12].img}
+          alt={props.items[12].alt}
+          title={props.items[12].title}
+          year={props.items[12].year}
+          info={props.items[12].info}
+          qr={props.items[12].qr}
+        />
+        {/* alt: "Brandon Alexander, Computer love", */}
+        <Artwork
+          hasVid={props.items[1].vid}
           artist={props.items[1].artist}
           image={props.items[1].img}
           alt={props.items[1].alt}
@@ -129,6 +221,51 @@ const Artworks = (props) => {
           info={props.items[1].info}
           qr={props.items[1].qr}
         />
+        {/* img: "LilyMott-4.jpg", */}
+        <Artwork
+          hasVid={props.items[8].vid}
+          artist={props.items[8].artist}
+          image={props.items[8].img}
+          alt={props.items[8].alt}
+          title={props.items[8].title}
+          year={props.items[8].year}
+          info={props.items[8].info}
+          qr={props.items[8].qr}
+        />
+        {/* alt: "Ray Weitzenberg - Harvey's Unicorn", */}
+        <Artwork
+          hasVid={props.items[15].vid}
+          artist={props.items[15].artist}
+          image={props.items[15].img}
+          alt={props.items[15].alt}
+          title={props.items[15].title}
+          year={props.items[15].year}
+          info={props.items[15].info}
+          qr={props.items[15].qr}
+        />
+        {/* alt: "Molly Devlin - The Fruting Body", */}
+        <Artwork
+          hasVid={props.items[10].vid}
+          artist={props.items[10].artist}
+          image={props.items[10].img}
+          alt={props.items[10].alt}
+          title={props.items[10].title}
+          year={props.items[10].year}
+          info={props.items[10].info}
+          qr={props.items[10].qr}
+        />
+        {/* img: "LilyMott.jpg", */}
+        <Artwork
+          hasVid={props.items[9].vid}
+          artist={props.items[9].artist}
+          image={props.items[9].img}
+          alt={props.items[9].alt}
+          title={props.items[9].title}
+          year={props.items[9].year}
+          info={props.items[9].info}
+          qr={props.items[9].qr}
+        />
+        {/* alt: "Brandon Gastinell, DOOM", */}
         <Artwork
           hasVid={props.items[2].vid}
           artist={props.items[2].artist}
@@ -139,23 +276,49 @@ const Artworks = (props) => {
           info={props.items[2].info}
           qr={props.items[2].qr}
         />
+        {/* alt: "Molly Devlin, fountainhead", */}
         <Artwork
-          artist={props.items[3].artist}
-          image={props.items[3].img}
-          alt={props.items[3].alt}
-          title={props.items[3].title}
-          year={props.items[3].year}
-          info={props.items[3].info}
-          qr={props.items[3].qr}
+          hasVid={props.items[11].vid}
+          artist={props.items[11].artist}
+          image={props.items[11].img}
+          alt={props.items[11].alt}
+          title={props.items[11].title}
+          year={props.items[11].year}
+          info={props.items[11].info}
+          qr={props.items[11].qr}
         />
+        {/* alt: "Ashley Regan, Tranquility", */}
         <Artwork
-          artist={props.items[4].artist}
-          image={props.items[4].img}
-          alt={props.items[4].alt}
-          title={props.items[4].title}
-          year={props.items[4].year}
-          info={props.items[4].info}
-          qr={props.items[4].qr}
+          hasVid={props.items[0].vid}
+          artist={props.items[0].artist}
+          image={props.items[0].img}
+          alt={props.items[0].alt}
+          title={props.items[0].title}
+          year={props.items[0].year}
+          info={props.items[0].info}
+          qr={props.items[0].qr}
+        />
+        {/* alt: "Jamie Worrall, At Elderway", */}
+        <Artwork
+          hasVid={props.items[7].vid}
+          artist={props.items[7].artist}
+          image={props.items[7].img}
+          alt={props.items[7].alt}
+          title={props.items[7].title}
+          year={props.items[7].year}
+          info={props.items[7].info}
+          qr={props.items[7].qr}
+        />
+        {/* alt: "Ray Weitzenberg - Halalrunner", */}
+        <Artwork
+          hasVid={props.items[14].vid}
+          artist={props.items[14].artist}
+          image={props.items[14].img}
+          alt={props.items[14].alt}
+          title={props.items[14].title}
+          year={props.items[14].year}
+          info={props.items[14].info}
+          qr={props.items[14].qr}
         />
       </Slider>
 
@@ -166,11 +329,25 @@ const Artworks = (props) => {
           ref={(slider2) => setNav2(slider2)}
           className="slider-thumbs"
         >
-          <ArtworkThumbnail image={props.items[0].img} />
-          <ArtworkThumbnail image={props.items[1].img} />
-          <ArtworkThumbnail image={props.items[2].thumb} />
+          <ArtworkThumbnail image={props.items[18].img} />
+          <ArtworkThumbnail image={props.items[16].img} />
           <ArtworkThumbnail image={props.items[3].img} />
+          <ArtworkThumbnail image={props.items[6].img} />
+          <ArtworkThumbnail image={props.items[13].img} />
           <ArtworkThumbnail image={props.items[4].img} />
+          <ArtworkThumbnail image={props.items[17].img} />
+          <ArtworkThumbnail image={props.items[5].img} />
+          <ArtworkThumbnail image={props.items[12].img} />
+          <ArtworkThumbnail image={props.items[1].img} />
+          <ArtworkThumbnail image={props.items[8].img} />
+          <ArtworkThumbnail image={props.items[15].img} />
+          <ArtworkThumbnail image={props.items[10].img} />
+          <ArtworkThumbnail image={props.items[9].img} />
+          <ArtworkThumbnail image={props.items[2].img} />
+          <ArtworkThumbnail image={props.items[0].img} />
+          <ArtworkThumbnail image={props.items[11].img} />
+          <ArtworkThumbnail image={props.items[7].img} />
+          <ArtworkThumbnail image={props.items[14].img} />
         </Slider>
       </div>
     </>
